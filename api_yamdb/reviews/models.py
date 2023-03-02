5df31d782f4b4be5f23c6236fbe44a1dd81366c7
Михаил Филенко <m@filenko.su>
2023-03-02 17:39:40 +0300

    cat.gen.tit.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,100 @@
 from django.db import models
 
 from users.models import User
-from .validators import validate_rating
+from .validators import validate_rating, validate_year
+
+
+class CategoryGenreAbstract(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Идентификатор',
+        max_length=50,
+        unique=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.title[:50]
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Category(CategoryGenreAbstract):
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Genre(CategoryGenreAbstract):
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Title(models.Model):
+
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256
+    )
+    year = models.IntegerField(
+        verbose_name='Дата выхода',
+        validators=[validate_year],
+        blank=True
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        verbose_name='Жанр',
+        through='GenreTitle'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE)
+    genre = models.ForeignKey(
+        Genre,
+        verbose_name='Жанр',
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Произведение "{self.title}", жанр: {self.genre}'
+
+    class Meta:
+        verbose_name = 'Произведение и жанр'
+        verbose_name_plural = 'Произведения и жанры'
 
 
 class ReviewCommentAbstract(models.Model):
@@ -60,95 +153,3 @@ class Comment(ReviewCommentAbstract):
     class Meta:
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
-
-
-class CategoryGenreAbstract(models.Model):
-    name = models.CharField(
-        verbose_name='Название',
-        max_length=256
-    )
-    slug = models.SlugField(
-        verbose_name='Идентификатор',
-        max_length=50,
-        unique=True,
-        blank=True
-    )
-
-    def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = self.title[:50]
-        super().save(*args, **kwargs)
-
-    class Meta:
-        abstract = True
-
-
-class Category(CategoryGenreAbstract):
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-
-
-class Genre(CategoryGenreAbstract):
-
-    class Meta:
-        verbose_name = 'Жанр'
-        verbose_name_plural = 'Жанры'
-
-
-class Title(models.Model):
-
-    name = models.CharField(
-        verbose_name='Название',
-        max_length=256
-    )
-    year = models.IntegerField(
-        verbose_name='Дата выхода',
-        blank=True
-    )
-    description = models.TextField(
-        verbose_name='Описание',
-        null=True,
-        blank=True
-    )
-    genre = models.ManyToManyField(
-        Genre,
-        verbose_name='Жанр',
-        through='GenreTitle'
-    )
-    category = models.ForeignKey(
-        Category,
-        verbose_name='Категория',
-        on_delete=models.SET_NULL,
-        related_name='titles',
-        null=True
-    )
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Произведение'
-        verbose_name_plural = 'Произведения'
-
-
-class GenreTitle(models.Model):
-    title = models.ForeignKey(
-        Title,
-        verbose_name='Произведение',
-        on_delete=models.CASCADE)
-    genre = models.ForeignKey(
-        Genre,
-        verbose_name='Жанр',
-        on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'Произведение "{self.title}", жанр: {self.genre}'
-
-    class Meta:
-        verbose_name = 'Произведение и жанр'
-        verbose_name_plural = 'Произведения и жанры'
--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,8 +1,16 @@
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 def validate_rating(value):
     if (value > 10) or (value < 1):
         raise ValidationError(
-            'Введите значение от 1 до 10'
+            'Введите значение от 1 до 10')
+
+
+def validate_year(year):
+    if year > timezone.now().year:
+        raise ValidationError(
+            ('%(value)s еще не наступил'),
+            params={'year': year},
         )
