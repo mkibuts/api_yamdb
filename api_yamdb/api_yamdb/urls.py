7b48dcd893f15261acb110f760b12e1c96dc565f
Михаил Филенко <m@filenko.su>
2023-03-02 16:25:08 +0300

    категор.жанр.тайтл прописал урлы

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('api_auth.urls', namespace='api_auth')),
+    path('api/', include('api.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
