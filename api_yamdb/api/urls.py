ae47c03cc515d85fcf4bfcd2ccf99e4b5c3d9d88
Михаил Филенко <m@filenko.su>
2023-03-07 19:34:10 +0300

    before merging with master

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
 from .views import (CategoryViewSet, GenreViewSet, TitleViewSet,
                     ReviewViewSet, CommentViewSet)
 from users.views import (RegistrationAPIView, UserActivateAPIView, UserViewSet,
@@ -17,9 +16,8 @@ router.register(
 router.register('users', UserViewSet)
 router.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet)
 router.register(r'titles', TitleViewSet, basename='titles')
-
 urlpatterns = [
-    path('v1/users/me/', UserMeAPIView.as_view()),
+    path('v1/users/me/', user_me),
     path('v1/users/<username>/', user_username),
     path('v1/auth/signup/', RegistrationAPIView.as_view()),
     path('v1/auth/token/', UserActivateAPIView.as_view()),
