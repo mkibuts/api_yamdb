94d606985fdaf00817913ca1af91bf5f1a1381b7
Сергей Куриленко <sv.kurick@gmail.com>
2023-03-09 18:44:08 +0300

    Перенес permissions, views, serializers из приложения users в приложение api

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -21,3 +21,17 @@ class AdminOrReadOnly(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.role == 'admin'
                 or request.user.is_superuser)
+
+
+class AdminOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated and (request.user.role == 'admin'
+                or request.user.is_staff))
+
+
+class AuthorOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.author
