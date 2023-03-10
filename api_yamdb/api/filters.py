20dbceb0df0aaba4d637e6f05a435f923683c6a6
krovekarp <98317860+krovekarp@users.noreply.github.com>
2023-03-10 20:16:39 +0400

    Update filters.py

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,16 +1,12 @@
 from django_filters import CharFilter
 from django_filters.rest_framework import FilterSet
-
 from reviews.models import Title
 
 
 class TitleFilter(FilterSet):
-    category = CharFilter(
-        field_name='category__slug'
-    )
-    genre = CharFilter(
-        field_name='genre__slug'
-    )
+    category = CharFilter(field_name='category__slug',)
+    genre = CharFilter(field_name='genre__slug',)
+    name = CharFilter(field_name='name',)
 
     class Meta:
         model = Title
